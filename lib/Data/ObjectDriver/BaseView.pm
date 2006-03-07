c414933c22305b9338bd564dc294993321b43d56
Tatsuhiko Miyagawa <miyagawa@sixapart.com>
2006-03-06 16:19:28 -0800

    Don't use HAVING if view class has datasource.  It's a nasty hack, yeah.  Bugzid:25491

--- lib/Data/ObjectDriver/BaseView.pm
+++ lib/Data/ObjectDriver/BaseView.pm
@@ -13,16 +13,19 @@ sub search {
     $args->{sql_statement} = $class->base_statement($terms, $args);
     $args = Storable::dclone($args);
 
-    my %cols = map { $_ => 1 } @{ $class->properties->{columns} }; 
-    my %having;
-    for my $key (keys %$terms) {
-        if ($cols{$key} && ! $args->{sql_statement}->has_where($key, $terms->{$key})) {
-            # Don't need to delete from $term, because D::OD ignores
-            # it anyway when used as View class
-            $having{$key} = $terms->{$key};
+    # quick hack: don't use HAVING if view class has datasource
+    if (! $class->properties->{datasource}) {
+        my %cols = map { $_ => 1 } @{ $class->properties->{columns} }; 
+        my %having;
+        for my $key (keys %$terms) {
+            if ($cols{$key} && $args->{sql_statement}->has_where($key, $terms->{$key})) {
+                # Don't need to delete from $term, because D::OD ignores
+                # it anyway when used as View class
+                $having{$key} = $terms->{$key};
+            }
         }
+        $args->{having} = \%having;
     }
-    $args->{having} = \%having;
     
     $class->_proxy('search', $terms, $args)
 }
