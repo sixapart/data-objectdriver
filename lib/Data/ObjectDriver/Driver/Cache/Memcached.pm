664c1338b159ea8aa9bf08d13c0555ca5cb70bbd
Ben Trott <ben@sixapart.com>
2006-03-27 11:31:03 -0800

    Memcached caching driver should use object's inflate/deflate before  caching the object.

--- lib/Data/ObjectDriver/Driver/Cache/Memcached.pm
+++ lib/Data/ObjectDriver/Driver/Cache/Memcached.pm
@@ -7,6 +7,18 @@ use base qw( Data::ObjectDriver::Driver::BaseCache );
 use Cache::Memcached;
 use Carp ();
 
+sub deflate {
+    my $driver = shift;
+    my($obj) = @_;
+    $obj->deflate;
+}
+
+sub inflate {
+    my $driver = shift;
+    my($class, $data) = @_;
+    $class->inflate($data);
+}
+
 sub add_to_cache            { shift->cache->add(@_)       }
 sub update_cache            { shift->cache->set(@_)       }
 sub remove_from_cache       { shift->cache->delete(@_)    }
