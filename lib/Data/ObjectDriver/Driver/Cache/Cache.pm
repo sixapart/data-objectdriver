bdfff16e6cc02172a5cbd32bc8fc1e96887c9d23
Yann Kerherve <yann@cyberion.net>
2007-04-02 18:50:11 +0000

    inflate/deflate structures during cache roundtrip

--- lib/Data/ObjectDriver/Driver/Cache/Cache.pm
+++ lib/Data/ObjectDriver/Driver/Cache/Cache.pm
@@ -6,6 +6,18 @@ use warnings;
 
 use base qw( Data::ObjectDriver::Driver::BaseCache );
 
+sub deflate {
+    my $driver = shift;
+    my($obj) = @_;
+    $obj->deflate;
+}
+
+sub inflate {
+    my $driver = shift;
+    my($class, $data) = @_;
+    $class->inflate($data);
+}
+
 sub get_from_cache    { shift->cache->thaw(@_)   }
 sub add_to_cache      { shift->cache->freeze(@_) }
 sub update_cache      { shift->cache->freeze(@_) }
