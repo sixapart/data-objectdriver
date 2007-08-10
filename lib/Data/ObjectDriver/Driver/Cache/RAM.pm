72aacbaa4ae3bbc4dfca349a0be72283853ea395
Mark Paschal <mpaschal@sixapart.com>
2007-08-10 18:57:58 +0000

    Hook ram cache driver operations into debug and profiler messages BugzID: 56565

--- lib/Data/ObjectDriver/Driver/Cache/RAM.pm
+++ lib/Data/ObjectDriver/Driver/Cache/RAM.pm
@@ -15,10 +15,58 @@ sub init {
     $driver->SUPER::init(%param);
 }
 
-sub get_from_cache    { $Cache{$_[1]}         }
-sub add_to_cache      { $Cache{$_[1]} = $_[2] }
-sub update_cache      { $Cache{$_[1]} = $_[2] }
-sub remove_from_cache { delete $Cache{$_[1]}  }
-sub clear_cache       { %Cache = ()           }
+sub get_from_cache {
+    my $driver = shift;
+
+    $driver->start_query('RAMCACHE_GET ?', \@_);
+    my $ret = $Cache{$_[0]};
+    $driver->end_query(undef);
+
+    return if !defined $ret;
+    return $ret;
+}
+
+sub add_to_cache {
+    my $driver = shift;
+
+    $driver->start_query('RAMCACHE_ADD ?', \@_);
+    my $ret = $Cache{$_[0]} = $_[1];
+    $driver->end_query(undef);
+
+    return if !defined $ret;
+    return $ret;
+}
+
+sub update_cache {
+    my $driver = shift;
+
+    $driver->start_query('RAMCACHE_SET ?', \@_);
+    my $ret = $Cache{$_[0]} = $_[1];
+    $driver->end_query(undef);
+
+    return if !defined $ret;
+    return $ret;
+}
+
+sub remove_from_cache {
+    my $driver = shift;
+
+    $driver->start_query('RAMCACHE_DELETE ?', \@_);
+    my $ret = delete $Cache{$_[0]};
+    $driver->end_query(undef);
+
+    return if !defined $ret;
+    return $ret;
+}
+
+sub clear_cache {
+    my $driver = shift;
+
+    $driver->start_query('RAMCACHE_CLEAR');
+    %Cache = ();
+    $driver->end_query(undef);
+
+    return;
+}
 
 1;
