70cc871df84f2dddbd54981fd47dc5306c050e6f
Yann Kerherve <yann@cyberion.net>
2007-11-10 21:31:48 +0000

    Emergency week-end fix for our $sth business

--- lib/Data/ObjectDriver/Driver/DBI.pm
+++ lib/Data/ObjectDriver/Driver/DBI.pm
@@ -113,7 +113,7 @@ sub fetch {
     $sql .= "\nFOR UPDATE" if $orig_args->{for_update};
     my $dbh = $driver->r_handle($class->properties->{db});
     $driver->start_query($sql, $stmt->{bind});
-    my $sth = $orig_args->{no_cached_prepare} ? $dbh->prepare($sql) : $dbh->prepare_cached($sql);
+    my $sth = $orig_args->{no_cached_prepare} ? $dbh->prepare($sql) : $dbh->prepare_cached($sql, {}, 3);
     $sth->execute(@{ $stmt->{bind} });
     $sth->bind_columns(undef, @bind);
 
