bf8c9881145e13ce307f65cfc60a3e322477a26f
Keita Jamadam Sugama <sugama@jamadam.com>
2025-10-02 12:18:59 +0900

    set username and password for test models for oracle

--- t/lib/sql/Blog.pm
+++ t/lib/sql/Blog.pm
@@ -7,11 +7,18 @@ use base 'Data::ObjectDriver::BaseObject';
 use Data::ObjectDriver::Driver::DBI;
 use DodTestUtil;
 
+my $username = DodTestUtil::env('DOD_TEST_USER', 'blog');
+my $password = DodTestUtil::env('DOD_TEST_PASS', 'blog');
+
 __PACKAGE__->install_properties({
     columns => ['id', 'parent_id', 'name'],
     datasource  => 'blog',
     primary_key => 'id',
-    driver      => Data::ObjectDriver::Driver::DBI->new(dsn => DodTestUtil::dsn('global')),
+    driver      => Data::ObjectDriver::Driver::DBI->new(
+        dsn => DodTestUtil::dsn('global'),
+        $username ? (username => $username) : (),
+        $password ? (password => $password) : (),
+    ),
 });
 
 1;
--- t/lib/sql/Entry.pm
+++ t/lib/sql/Entry.pm
@@ -7,11 +7,18 @@ use base 'Data::ObjectDriver::BaseObject';
 use Data::ObjectDriver::Driver::DBI;
 use DodTestUtil;
 
+my $username = DodTestUtil::env('DOD_TEST_USER', 'entry');
+my $password = DodTestUtil::env('DOD_TEST_PASS', 'entry');
+
 __PACKAGE__->install_properties({
     columns => ['id', 'blog_id', 'title', 'text'],
     datasource  => 'entry',
     primary_key => 'id',
-    driver      => Data::ObjectDriver::Driver::DBI->new(dsn => DodTestUtil::dsn('global')),
+    driver      => Data::ObjectDriver::Driver::DBI->new(
+        dsn => DodTestUtil::dsn('global'),
+        $username ? (username => $username) : (),
+        $password ? (password => $password) : (),
+    ),
 });
 
 1;
