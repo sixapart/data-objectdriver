23bb56130a44517859c1a702a7cb12fad60e06e2
Kenichi Ishigaki <ishigaki@cpan.org>
2026-02-06 19:04:18 +0900

    Add bind_param_attributes to ::DBD::MariaDB

--- lib/Data/ObjectDriver/Driver/DBD/MariaDB.pm
+++ lib/Data/ObjectDriver/Driver/DBD/MariaDB.pm
@@ -7,4 +7,16 @@ use base qw( Data::ObjectDriver::Driver::DBD::mysql );
 
 sub fetch_id { $_[3]->{mariadb_insertid} || $_[3]->{insertid} }
 
+sub bind_param_attributes {
+    my ($dbd, $data_type) = @_;
+    if ($data_type) {
+        if ($data_type eq 'blob') {
+            return DBI::SQL_BINARY;
+        } elsif ($data_type eq 'binchar') {
+            return DBI::SQL_BINARY;
+        }
+    }
+    return;
+}
+
 1;
